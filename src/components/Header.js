f18025213b431eb87232269dceafbc7d7f6d5118
Prithvidhar reddy Pudu <PrithviDharpudu@hotmail.com>
2023-08-06 19:44:25 -0500

    first

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    
+    <section className='bg-yellow-400 absolute top-0 flex justify-around w-screen'>
+        <h1 className='text-gray-700 text-lg'>TV Nostradamus</h1>
+        <h4 className='text-gray-700 text-lg'>An App by Prithvidhar Pudu</h4>
+    </section>
+
+  )
+}
+
+export default Header
