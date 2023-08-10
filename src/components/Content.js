484202596929013a1ec8a904c357745829c73b44
Prithvidhar reddy Pudu <PrithviDharpudu@hotmail.com>
2023-08-09 21:30:09 -0500

    added content template

---
+++ src/components/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import nostradamus from "../../src/nostradamus.jpg"
+
+const Content = () => {
+  return (
+    <div className='grid grid-cols-2 h-full mt-20 gap-2 p-2 leading-10 grid-rows-15 gap-x-4'>
+        <p className='text-white text-3xl col-start-1 col-end-2 text-center font-anton row-span-1'>Welcome to sanctum of TV Nostradamus</p>
+        <p className='text-white text-2xl col-start-1 col-end-2 font-anton leading-loose text-center row-span-2 place-self-center'>Thousands of shows and movies are being produced. What to watch?, the choice is overbearing. Will your favourite show die just as you're am getting invested?
+            Enter the name of your show/movie to get a prediction of its rating over the next few years to help ease your frustrations
+        </p>
+        <input type='text' placeholder='Enter your show here...' className='  col-start-1 col-end-2 row-span-2 text-3xl h-full rounded-full p-5 bg-yellow-400'></input>
+        <img className="h-full w-3/4 row-start-1 row-span-5 col-start-2 col-end-3 place-self-center" alt='Nostradamus and a TV' src={nostradamus}></img>
+    </div>
+  )
+}
+
+export default Content
